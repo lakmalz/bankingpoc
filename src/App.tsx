3f9e3e2706c9270d8bd9ae421859b7a404e94f05
Lakmal Weerasekara <alrweerasekara@gmail.com>
2025-04-07 15:05:29 +0800

    sample styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import { Box, Container } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 
 function App() {
   return (
-    <Container
-      sx={{
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Box sx={{width:{ xs:1, md:200},height:200, bgcolor:"tomato"}}></Box>
+    <Container>
     </Container>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#013e87",
+    },
+    secondary: {
+      main: "#2e74c9",
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: "3rem",
+      fontWeight: 600,
+    },
+    h2: {
+      fontSize: "1.75rem",
+      fontWeight: 600,
+    },
+    h3: {
+      fontSize: "1.5rem",
+      fontWeight: 600
+    },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
